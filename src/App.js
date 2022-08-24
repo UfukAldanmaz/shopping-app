0d55c4535683e80a9845fa5d00c99af8400cb770
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-08-24 23:55:29 +0300

    Apply routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Home from './components/Home';
+import ProductDetail from './Pages/ProductDetail'
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div className='App'><Home /></div>} />
+        <Route path=":id" element={<ProductDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
