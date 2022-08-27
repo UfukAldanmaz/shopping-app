0082a03b8097f869cad5bf2c2e5a7890f2e3f34c
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-08-27 14:17:42 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import Home from './components/Home';
 import ProductDetail from './Pages/ProductDetail'
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { DataProvider } from './context/DataContext';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<div className='App'><Home /></div>} />
-        <Route path=":id" element={<ProductDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <DataProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<div className='App'><Home /></div>} />
+          <Route path=":id" element={<ProductDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </DataProvider>
   );
 }
 
