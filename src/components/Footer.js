e35c1c1d92afe5cdfa5425892fa98f8ed4f3051b
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-10-26 17:56:44 +0300

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return <div className="footer-container">
+        <p>About Us</p>
+        <p>Contact</p>
+        <ul><b>Help</b>
+            <li>
+                Your Account
+            </li>
+            <li>
+                Your Orders
+            </li>
+        </ul>
+    </div>
+}
+
+export default Footer;
