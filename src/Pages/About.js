7bdcd4a39d03456293a3d56039ff355d6c6fccd4
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-10-26 16:54:56 +0300

    Create About.js

---
+++ src/Pages/About.js
@@ -0,0 +1,19 @@
+const About = () => {
+    return <div>
+        <h1>Who are we?</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin placerat rutrum ligula in aliquet. Nam iaculis pulvinar erat, a pretium lacus pharetra non. Maecenas aliquet nibh ac neque auctor euismod. Pellentesque ultrices ipsum vitae lorem facilisis, ut ornare ipsum lobortis. Quisque dignissim magna in ultricies tempus. Vivamus et lectus neque. Vestibulum ullamcorper ante a augue pretium, non iaculis ex luctus.
+
+            Aliquam at iaculis est, vitae malesuada sem. Duis iaculis ex nec vehicula congue. Sed porta diam eleifend mi elementum facilisis. Nulla ultricies libero gravida nunc feugiat, quis pulvinar odio ornare. Nulla facilisi. Donec at nisi mi. Praesent bibendum lacinia velit, vel posuere diam efficitur at. Integer orci nulla, vestibulum eu sagittis tempus, molestie a est. Nulla consectetur non erat eu tristique. Phasellus eu eros ac sapien auctor euismod eget vel nibh. Sed eu lacus metus. Donec at ligula bibendum, convallis nunc id, cursus mauris.
+
+            Donec vulputate orci eget magna fringilla fermentum. Pellentesque vulputate gravida hendrerit. Fusce ut suscipit libero. Proin non condimentum lorem. Duis nec tincidunt ex. Vivamus tempus vel ex suscipit semper. Donec tincidunt, eros vitae ultrices suscipit, odio nisl condimentum ligula, nec iaculis tellus quam quis lorem. Praesent tempus erat at nunc ullamcorper euismod. Curabitur ante odio, interdum non convallis eget, dapibus eu nibh. Vestibulum mollis tristique erat, eget dictum felis mollis eu. Ut egestas imperdiet augue sit amet auctor. Nam quis rhoncus nulla. Morbi nec nisl odio. Vestibulum posuere, enim tincidunt semper dictum, ipsum eros dapibus metus, nec dapibus arcu odio vel lectus. Mauris sit amet libero sit amet orci efficitur semper eu vel diam.
+
+            Donec malesuada enim vehicula, varius massa at, pretium urna. Curabitur laoreet sodales massa. Nunc at quam quis diam viverra luctus. Pellentesque eu dolor nisi. Vivamus placerat magna et enim rhoncus efficitur. In elementum at turpis a mollis. Nullam a risus nunc. Duis fringilla ultricies sapien et convallis. Integer eget nisi sit amet ex maximus posuere. Sed augue lorem, varius a efficitur at, lobortis vitae diam. Maecenas ultricies scelerisque orci, at feugiat lectus dignissim vel. Etiam cursus felis et tristique cursus. Curabitur faucibus massa sed hendrerit pellentesque.
+
+            Aenean eu egestas sapien. Donec aliquam velit eu diam porta, sed pharetra nunc maximus. Vivamus nec purus eget neque elementum posuere. In hac habitasse platea dictumst. Sed congue auctor velit, id hendrerit diam imperdiet a. Nullam dictum felis ac sapien luctus placerat. Aenean eu enim suscipit, consectetur leo ut, rhoncus augue. Quisque dictum sollicitudin pretium. Mauris finibus porta suscipit. Proin lectus velit, malesuada ac neque sit amet, egestas pretium mauris. Nunc ex nisl, hendrerit at maximus tincidunt, iaculis ac massa. Suspendisse dignissim orci nulla, in viverra nulla tempus in. In dignissim est ut eros lacinia, at sollicitudin libero tempus. Etiam tortor velit, tincidunt sit amet odio id, tempus suscipit neque. In scelerisque nisl diam, id maximus justo sagittis sit amet.
+        </p>
+
+    </div>
+}
+
+export default About;
