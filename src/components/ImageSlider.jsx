4ccc15f11c6157718860e68ea9952344aa13eb67
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-10-28 18:06:45 +0300

    Create ImageSlider.jsx

---
+++ src/components/ImageSlider.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { images } from "../data/imageData";
+
+const ImageSlider = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    return (
+        <div className="slider">
+
+            <div className="slides" style={{ backgroundImage: `url(${images[currentIndex].img})` }}>
+                <div onClick={() => {
+                    currentIndex > 0 && setCurrentIndex(currentIndex - 1)
+                }} style={{ fontSize: "100px", float: "left" }}>
+                    ﹤
+                </div>
+
+                <div className="slider-text">
+                    <h1>{images[currentIndex].title}</h1>
+                    <p>{images[currentIndex].subtitle}</p>
+                </div>
+
+                <div onClick={() => {
+                    currentIndex < images.length - 1 && setCurrentIndex(currentIndex + 1)
+                }} style={{ fontSize: "100px", float: "right" }}>
+                    ﹥
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ImageSlider;
