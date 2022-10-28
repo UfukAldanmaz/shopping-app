b7f024452757f4e2eec91f51ad32244990e87880
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-10-28 16:06:06 +0300

    Create Products.js

---
+++ src/Pages/Products.js
@@ -0,0 +1,32 @@
+import { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import ShoppingItem from "../components/ShoppingItem";
+import DataContext from "../context/DataContext";
+
+const Products = () => {
+    const { filteredData } = useContext(DataContext);
+    const [list, setList] = useState(filteredData);
+    const { category } = useParams();
+    const filteredList = () => {
+        if (!category) {
+            return filteredData;
+        }
+
+        return filteredData.filter((item) => item.category === category);
+
+    }
+
+    useEffect(() => {
+        console.log(category, filteredData.length);
+        const selectedData = filteredList();
+        setList(selectedData);
+    }, [filteredData])
+
+
+    return <div id="shopping-list">{list.map(item =>
+        <ShoppingItem key={item.id} item={item} />
+    )}
+    </div>
+}
+
+export default Products;
