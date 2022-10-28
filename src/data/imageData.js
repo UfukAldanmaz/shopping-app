64fca049edcff32dac26a07606dc47a71638dcb4
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-10-28 18:07:11 +0300

    Create imageData.js

---
+++ src/data/imageData.js
@@ -0,0 +1,10 @@
+import Image1 from "../images/all.png"
+import Image2 from "../images/men.jpg"
+import Image3 from "../images/women.jpg"
+
+export const images = [
+    { title: "All categories", subtitle: "Shop now", img: Image1, },
+    { title: "Sale in Men", subtitle: "Buy something", img: Image2, },
+    { title: "Sale in Women", subtitle: "Buy Everything", img: Image3, },
+
+];
