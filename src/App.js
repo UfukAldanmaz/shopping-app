0ab4b3a6182051d1488af17b3e66be2a911fa281
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-10-28 16:04:34 +0300

    Add product route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import Home from './components/Home';
 import ProductDetail from './Pages/ProductDetail'
 import './App.css';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { DataProvider } from './context/DataContext';
 import Navbar from './components/Navbar';
 import About from './Pages/About';
 import Footer from './components/Footer';
+import Products from './Pages/Products';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <DataProvider>
-      <HashRouter>
+      <BrowserRouter>
         <Navbar />
+        <Sidebar />
         <Routes>
           <Route path="/" element={<div className='App'><Home /></div>} />
           <Route path=":id" element={<ProductDetail />} />
           <Route path="/about" element={<About />} />
+          <Route path='/products'>
+            <Route path=':category' element={<Products />} />
+            <Route path='' element={<Products />} />
+          </Route>
         </Routes>
         <Footer />
-      </HashRouter>
+      </BrowserRouter>
     </DataProvider>
   );
 }
