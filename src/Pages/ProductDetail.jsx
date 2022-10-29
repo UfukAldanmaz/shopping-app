410f848deabb4d144a49e057834a8988184d6fed
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-10-29 16:30:10 +0300

    Add add to cart function

--- src/Pages/ProductDetail.jsx
+++ src/Pages/ProductDetail.jsx
@@ -5,7 +5,7 @@ import DataContext from "../context/DataContext";
 
 const ProductDetail = () => {
     const [data, setData] = useState({});
-    const { loading, setLoading } = useContext(DataContext);
+    const { loading, setLoading, addToBasket, removeFromBasket, getProduct } = useContext(DataContext);
     const navigate = useNavigate();
     const { id } = useParams();
 
@@ -22,7 +22,29 @@ const ProductDetail = () => {
 
     useEffect(() => {
         getData();
-    });
+    }, []);
+
+    let first = <button className="add-cart-detail" onClick={() => {
+        addToBasket(id);
+    }}>Add to Cart</button>
+
+    let second = <div className="add-btn-on-card-detail">
+        <button className="plus-minus-detail" onClick={() => removeFromBasket(id)}>➖</button>
+        <p className="quantity-on-card-detail">{getProduct(id.id)?.quantity}</p>
+        <button className="plus-minus-detail" onClick={() => {
+            addToBasket(id);
+        }}>➕</button>    </div>
+
+    const getProductInBasket = (product) => {
+        const found = getProduct(product.id)
+        if (found === undefined || found.quantity === 0) {
+            return first;
+        } else if (found.quantity >= 1) {
+            return second;
+        }
+    }
+
+
     return (loading ? <p>Loading...</p> :
         <div className="detail-container">
             <div className="detail-img"> <img width={100} src={data.image} alt={data.title} />
@@ -31,8 +53,10 @@ const ProductDetail = () => {
                 <p className="detail-price">Price : ${data.price}</p>
                 <p className="detail-description">{data.description}</p>
                 <button className="detail-back-btn" onClick={() => navigate("/")}>Go back</button>
-            </div>
 
+            </div>
+            {/* <button>Add to Cart</button> */}
+            {getProductInBasket(id)}
         </div>
     );
 }
